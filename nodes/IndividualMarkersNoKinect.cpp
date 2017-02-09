925d77c603bf3e495ca9790c734fd79093231dfa
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2017-02-08 17:58:28 -0800

    [fix] #93 for IndividualMarkersNoKinect.cpp.

--- nodes/IndividualMarkersNoKinect.cpp
+++ nodes/IndividualMarkersNoKinect.cpp
@@ -72,7 +72,7 @@ double max_track_error;
 std::string cam_image_topic;
 std::string cam_info_topic;
 std::string output_frame;
-int marker_resolution = 7; // default marker resolution
+int marker_resolution = 5; // default marker resolution
 int marker_margin = 2; // default marker margin
 
 void getCapCallback (const sensor_msgs::ImageConstPtr & image_msg);
